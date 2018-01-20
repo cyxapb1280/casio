1965ab996ff28c6ab992880b37c04ba5a6d9754a
cyxapb1280 <suhanoff8888@gmail.com>
2018-01-21 01:15:52 +0200

    appearance

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -2,45 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { NUMBER, OPERATOR } from '../reducers';
+import { NUMBER, BINARY_OPERATOR, OPERATOR } from '../reducers';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/merge';
+import { KEYS } from './keys';
 
 export interface CalcState {
   operandA: number;
   operandB: number;
-  currentOperand: string,
+  currentOperand: string;
   operator: string;
   display: number;
+  enabled: boolean;
 }
 
 interface AppState {
-  calc: CalcState
+  calc: CalcState;
 }
 
 @Component({
   selector: 'calc',
   templateUrl: './calc.component.html',
-  styleUrls: ['./calc.component.css']
+  styleUrls: ['./calc.component.scss']
 })
 export class CalcComponent implements OnInit {
-  number$ = new Subject().map((value) => ({ type: NUMBER, payload: Number(value) }));
-  operator$ = new Subject().map((value) => ({ type: OPERATOR, payload: value }));
+  firstSectionKeys = KEYS.slice(0, 2);
+  mainSectionKeys = KEYS.slice(2, 17);
+  lastSectionKeys = KEYS.slice(17, 25);
+  tallKey = KEYS[KEYS.length - 1];
+
+  keyDown$ = new Subject().map(({ type, value }) => ({ type: type, payload: value }));
 
   display$;
 
   constructor(private store: Store<AppState>) {
-    this.display$ = store.select<number>(state => { console.log(state.calc); return state.calc.display;});
+    this.display$ = store.select<number>(state => {
+      console.log(state.calc);
+      return state.calc.display;
+    });
 
-    this.display$.subscribe(state => console.log(state));
-
-    Observable.merge(this.number$, this.operator$)
-      .subscribe((action) => {
-        store.dispatch(action);
-      });
+    this.keyDown$.subscribe((action) => {
+      store.dispatch(action);
+    });
   }
 
   ngOnInit() {
   }
-
 }
---
+++ src/app/calc/keys.ts
@@ -0,0 +1,36 @@
+import { BINARY_OPERATOR, CLEAR, CLEAR_ALL, NUMBER, OFF, OPERATOR, RESULT } from '../reducers';
+
+interface Key {
+  label: string;
+  value: string;
+  type: string;
+}
+
+export const KEYS: Array<Key> = [
+  { label: '&radic;', value: 'sqrt', type: OPERATOR },
+  { label: 'OFF', value: 'off', type: OFF },
+  { label: 'MC', value: 'mc', type: OPERATOR },
+  { label: 'MR', value: 'mr', type: OPERATOR },
+  { label: 'M-', value: 'mMinus', type: OPERATOR },
+  { label: 'M+', value: 'mPlus', type: OPERATOR },
+  { label: '&divide;', value: 'divide', type: BINARY_OPERATOR },
+  { label: '&#37;', value: 'percent', type: OPERATOR},
+  { label: '7', value: '7', type: NUMBER },
+  { label: '8', value: '8', type: NUMBER },
+  { label: '9', value: '9', type: NUMBER },
+  { label: '&times;', value: 'multiply', type: BINARY_OPERATOR},
+  { label: '&plusmn;', value: 'sign', type: OPERATOR },
+  { label: '4', value: '4', type: NUMBER },
+  { label: '5', value: '5', type: NUMBER },
+  { label: '6', value: '6', type: NUMBER },
+  { label: '-', value: 'minus', type: BINARY_OPERATOR },
+  { label: 'C', value: 'clear', type: CLEAR },
+  { label: '1', value: '1', type: NUMBER },
+  { label: '2', value: '2', type: NUMBER },
+  { label: '3', value: '3', type: NUMBER },
+  { label: 'AC', value: 'clearAll', type: CLEAR_ALL },
+  { label: '0', value: '0', type: NUMBER },
+  { label: '&middot;', value: 'dot', type: '' },
+  { label: '=', value: 'equal', type: RESULT },
+  { label: '+', value: 'plus', type: BINARY_OPERATOR },
+];
